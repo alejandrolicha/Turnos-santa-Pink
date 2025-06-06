89734ec73b8864683250acccda59c051394000e3
alejandrolicha <alejandrohepper@gmail.com>
2025-06-06 19:43:24 -0300

    Update Sw.js

--- Sw.js
+++ Sw.js
@@ -1,8 +1,8 @@
 const CACHE_NAME = 'santa-pink-cache-v1';
 const urlsToCache = [
-  '.',
-  'index.html',
-  'manifest.json',
+  '/Turnos-santa-Pink/',
+  '/Turnos-santa-Pink/index.html',
+  '/Turnos-santa-Pink/manifest.json',
   'https://cdn.tailwindcss.com',
   'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
@@ -22,7 +22,6 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Si la respuesta está en el caché, la devuelve. Si no, la busca en la red.
         if (response) {
           return response;
         }
