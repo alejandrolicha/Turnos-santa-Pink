f063a097866ebc7a80946c4e91164833bf71fe37
alejandrolicha <alejandrohepper@gmail.com>
2025-06-06 19:34:18 -0300

    Create Sw.js

---
+++ Sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'santa-pink-cache-v1';
+const urlsToCache = [
+  '.',
+  'index.html',
+  'manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache abierto y listo.');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Si la respuesta está en el caché, la devuelve. Si no, la busca en la red.
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
